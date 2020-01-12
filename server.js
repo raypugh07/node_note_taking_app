fe5d7ad9d861d78fc7edea22d6a45e6f6672f27a
Your Name <you@example.com>
2020-01-12 16:36:15 -0600

    added config folder,/config

--- server.js
+++ server.js
@@ -1,22 +1,21 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 
-//create express app
+// create express app
 const app = express();
 
-//parse requestsof content-type - application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({ extended = true}))
+// parse requests of content-type - application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: true }))
 
-//parse requests of content-type - application/json
+// parse requests of content-type - application/json
 app.use(bodyParser.json())
 
-//define a simple route
+// define a simple route
 app.get('/', (req, res) => {
     res.json({"message": "Welcome to EasyNotes application. Take notes quickly. Organize and keep track of all your notes."});
-
 });
 
-//listen for requests
-app.listen(3000, ()=> {
-    console.log("Server listening on port 3000");
+// listen for requests
+app.listen(3000, () => {
+    console.log("Server is listening on port 3000");
 });
