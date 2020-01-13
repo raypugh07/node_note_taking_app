b80676a0401f8bc33ebe3041e0e34c835e5c258b
Your Name <you@example.com>
2020-01-12 21:41:38 -0600

    added db config and connection working,database.config.js

---
+++ config/database.config.js
@@ -0,0 +1,5 @@
+//imported into server.js to connect db using mongoose
+
+module.exports = {
+    url: 'mongodb://localhost:27017/easy-notes'
+}
--- server.js
+++ server.js
@@ -10,6 +10,22 @@ app.use(bodyParser.urlencoded({ extended: true }))
 // parse requests of content-type - application/json
 app.use(bodyParser.json())
 
+//configuring the db
+const dbConfig = require('./config/database.config.js');
+const mongoose = require('mongoose');
+
+mongoose.Promise = global.Promise;
+
+//connecting to db
+mongoose.connect(dbConfig.url, {
+    useNewUrlParser: true
+}).then(()=> {
+    console.log("connected to db");
+}).catch(err => {
+    console.log('could not connect to db.exitingnow...', err);
+    process.exit();
+});
+
 // define a simple route
 app.get('/', (req, res) => {
     res.json({"message": "Welcome to EasyNotes application. Take notes quickly. Organize and keep track of all your notes."});
